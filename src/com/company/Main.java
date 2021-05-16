67fcb2735c2a3693189d077a7ba0e071660f7c9c
Vladimir <ovodenko@gmail.com>
2021-05-16 22:06:06 +0300

    5rd commit b

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,14 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        // this if first commit in master
-        // first commit in branch
-        // first commit in branch
-        // second commit in master
-        // second commit branch ++
-        // 3 rd commit
-        // 4 th commit + master
-        // 5 th commit
-        // 6 th master
+        // all deleted master
     }
 }
